12dd2d9924fe6ec2f7ec043e7e5b76c9ea0b87b2
massakai <masashi.sakai1986@gmail.com>
2013-09-11 07:56:04 +0900

    recreate files for shopping app (depot)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '131b896dd57bd1866160e43139e08aec5f4a8d2f841ed698b4f9ee17d1742470e048d83a722580d55d61b37ad62c0c3c3b31763def188f4760ef5eee9f10f069'
